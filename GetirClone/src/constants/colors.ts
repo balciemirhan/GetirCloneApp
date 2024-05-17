bd29d58bbf617a0df90b71490a94d58b8aea5a62
Emirhan balci <balciemirhan@hotmail.com>
2024-05-17 19:02:32 +0300

    Project directory structure created

---
+++ GetirClone/src/constants/colors.ts
@@ -0,0 +1,21 @@
+type Colors = {
+  primary: string;
+  secondary: string;
+  background: string;
+  text: string;
+  error: string;
+  warning: string;
+  success: string;
+};
+
+const colors: Colors = {
+  primary: "#3498db",
+  secondary: "#2ecc71",
+  background: "#ecf0f1",
+  text: "#2c3e50",
+  error: "#e74c3c",
+  warning: "#f39c12",
+  success: "#2ecc71",
+};
+
+export default colors;
